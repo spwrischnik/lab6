98a23c10ee9ab7837fcdaf132b7752a374462da7
EC2 Default User <ec2-user@ip-172-31-44-178.us-east-2.compute.internal>
2019-10-21 21:36:42 +0000

    initial

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+app.engine('html', require('ejs').renderFile);
+
+//routes
+app.get("/", function(req, res)
+{
+   res.render("index.html"); 
+});
+app.get("/mercury", function(req, res)
+{
+    res.send("Mercury web page");
+});
+app.get("/venus", function(req, res)
+{
+    res.send("Venus web page");
+});
+
+//server listener
+app.listen(process.env.PORT, process.env.IP, function()
+{
+    console.log("express is running");
+});
+
